420a8d95caf44b64446bdba49498d089a4cd6105
SoupBuddies <98256557+soupbuddies@users.noreply.github.com>
2023-10-27 05:52:31 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>QQドメイン | オージーネットワークによる。</title>
+  </head>
+  <body>
+  <center>
+    <h1>QQドメイン | オージーネットワークによる。</h1>
+    <hr>
+    <br>
+    <p>サブドメインを選択し、「次へ」をクリックします。</p>
+    <select>
+    <option disabled><center>無料！</option>
+    <option selected><center>.qq.aussie.tk</option>
+    </select>
+    <br>
+    <br>
+    <button><a href="https://qq.aussie.tk/startpage/">Next</a></button>
+    <br>
+    <hr>
+    <h6>ⓒ 2023, QQドメイン | オージーネットワークによる。
+</h6>
+  </body>
+</html>
